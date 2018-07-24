724b33010c1f39a8b870e1f06c2997749d27a6c4
pvhumphr <pvhumphr@usc.edu>
2018-07-23 21:57:58 -0700

    Added Trend, Seasonality, and Forecast.

--- python/forecast_trips.py
+++ python/forecast_trips.py
@@ -23,8 +23,6 @@ from sklearn.linear_model import LinearRegression
 from statsmodels.tsa.seasonal import seasonal_decompose
 from datetime import datetime
 
-print('Start forecast_trips.py')
-
 # Connect to local Postgres database if working from workstation or laptop.
 # Otherwise connect to Postgres datase hosted by AWS/RDS.
 def connect_to_postgres():
@@ -82,19 +80,17 @@ finally:
 
 
 # Load selected data into a dataframe.
-df = pd.DataFrame(table_data, columns=['Trip Year', 'Trip Month', 'Trips'])
+df = pd.DataFrame(table_data, columns=['trip_year', 'trip_month', 'trips'])
 df
 
 # Add Trip Timestamp based on Trip Year and Trip Month.
 # Trip Timestamp is needed for Tred and Seasonal decomposition.
 
 for index, row in df.iterrows():
-    ty = df.loc[index, 'Trip Year'].astype(str)
-    tm = df.loc[index, 'Trip Month'].astype(str)
+    ty = df.loc[index, 'trip_year'].astype(str)
+    tm = df.loc[index, 'trip_month'].astype(str)
     wtf = ty + tm
-    print('About to call pd.to_datetime.')
-    df.loc[index, 'Trip Timestamp'] = pd.to_datetime(wtf, format="%Y%m")
-    print('Called pd.to_datetime.')
+    df.loc[index, 'trip_timestamp'] = pd.to_datetime(wtf, format="%Y%m")
 
 
 # df['Trip Timestamp'] = pd.to_datetime(df['Trip Year'].astype(str) + df['Trip Month'].astype(str), format='%Y%m')
@@ -103,11 +99,11 @@ for index, row in df.iterrows():
 # for index, row in df.iterrows():
 #     df.loc[index, 'Trip Timestamp'] = datetime.strptime(df.loc[index, 'Trip Year'].astype(str) + df.loc[index, 'Trip Month'].astype(str), '%Y%m')
 
-df1 = df.set_index('Trip Timestamp')
+df1 = df.set_index('trip_timestamp')
 
 
 # Decompose the Trips data into Trend and Seasonal Series.
-series = df1['Trips']
+series = df1['trips']
 result = seasonal_decompose(series, model='multiplicative')
 
 trend_array = result.trend.get_values()
@@ -121,7 +117,7 @@ seasonal_series = pd.Series(seasonal_array)
 # Create a new dataframe from the orignal data, Trend, and Seasonal Series.
 # Decomposition does not calculate the first and last six periods.
 df_extended = pd.concat([df, trend_series, seasonal_series], axis=1)
-df_extended = df_extended.rename(columns={0:'Trend', 1:'Seasonal'})
+df_extended = df_extended.rename(columns={0:'trend', 1:'seasonal'})
 df_extended = df_extended[6:-6]
 df_extended = df_extended.reset_index(col_level=0)
 df_extended = df_extended.drop(['index'], axis=1)
@@ -132,7 +128,7 @@ X = df_extended.index
 X = np.array(X)
 X = X.reshape(-1, 1)
 
-y = np.array(df_extended['Trend'])
+y = np.array(df_extended['trend'])
 model = LinearRegression()
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 model.fit(X_train, y_train)
@@ -145,7 +141,7 @@ X_predict = np.array(X_predict)
 X_predict = X_predict.reshape(-1,1)
 y_predict = model.predict(X_predict)
 y_predict = y_predict.astype(int)
-y_predict = pd.Series(y_predict, name='Predict', index=pd.Index(range(48,60,1)))
+y_predict = pd.Series(y_predict, name='predict', index=pd.Index(range(48,60,1)))
 y_predict 
 
 
@@ -153,46 +149,46 @@ y_predict
 # Trend Prediction Series.
 df_final = pd.concat([df_extended, y_predict], axis=1)
 df_final = df_final.fillna(0)
-df_final = df_final.drop(columns=['Trip Timestamp'])
+df_final = df_final.drop(columns=['trip_timestamp'])
 
 
 # For the additional periods, set Trip Year and Trip Month.
 previous_year = 0
 previous_month = 0
 for index, row in df_final.iterrows():
-    if row['Trip Year'] == 0:
+    if row['trip_year'] == 0:
         if previous_month == 12:
             previous_year += 1
             previous_month = 1
         else:
             previous_month += 1
-        df_final.loc[index,'Trip Year'] = previous_year
-        df_final.loc[index, 'Trip Month'] = previous_month
+        df_final.loc[index,'trip_year'] = previous_year
+        df_final.loc[index, 'trip_month'] = previous_month
     else:
-        previous_year = row['Trip Year']
-        previous_month = row['Trip Month']
+        previous_year = row['trip_year']
+        previous_month = row['trip_month']
 
 
 # For the additional periods, set Seasonal factor to the Seasonal factor from 
 # the previous year.
 for index, row in df_final.iterrows():
-    if df_final.loc[index, 'Seasonal'] == 0:
-        df_final.loc[index, 'Seasonal'] = df_final.loc[index - 12, 'Seasonal']
+    if df_final.loc[index, 'seasonal'] == 0:
+        df_final.loc[index, 'seasonal'] = df_final.loc[index - 12, 'seasonal']
 
 
 # For additional periods, set Trend to Predict.
 for index, row in df_final.iterrows():
-    if row['Trend'] == 0:
-        df_final.loc[index, 'Trend'] = df_final.loc[index, 'Predict']
+    if row['trend'] == 0:
+        df_final.loc[index, 'trend'] = df_final.loc[index, 'predict']
 
 
 # For additional periods, alculate Trips by multiplying Trend and Seasonl when 
 # Trips is zero.
 for index, row in df_final.iterrows():
-    if row['Trips'] == 0:
-        df_final.loc[index, 'Trips'] = df_final.loc[index, 'Trend'] * df_final.loc[index, 'Seasonal']
+    if row['trips'] == 0:
+        df_final.loc[index, 'trips'] = df_final.loc[index, 'trend'] * df_final.loc[index, 'seasonal']
 integer_fields = list(df_final.columns.values)
-integer_fields.remove('Seasonal')
+integer_fields.remove('seasonal')
 df_final[integer_fields] = df_final[integer_fields].astype(int)
 
 
@@ -201,9 +197,9 @@ def get_linear_regression():
     value_list = []
     for index, row in df_final.iterrows():
         value_entry = {}
-        value_entry['trip_year'] = int(df_final.loc[index, 'Trip Year'])
-        value_entry['trip_month'] = int(df_final.loc[index, 'Trip Month'])
-        value_entry['trips'] = int(df_final.loc[index, 'Trend'])
+        value_entry['trip_year'] = int(df_final.loc[index, 'trip_year'])
+        value_entry['trip_month'] = int(df_final.loc[index, 'trip_month'])
+        value_entry['trips'] = int(df_final.loc[index, 'trend'])
         value_list.append(value_entry)
 
     json_dict = {}
@@ -219,9 +215,9 @@ def get_seasonal_factors():
     value_list = []
     for index, row in df_final.iterrows():
         value_entry = {}
-        value_entry['trip_year'] = int(df_final.loc[index, 'Trip Year'])
-        value_entry['trip_month'] = int(df_final.loc[index, 'Trip Month'])
-        value_entry['seasonal_factor'] = float(df_final.loc[index, 'Seasonal'])
+        value_entry['trip_year'] = int(df_final.loc[index, 'trip_year'])
+        value_entry['trip_month'] = int(df_final.loc[index, 'trip_month'])
+        value_entry['seasonal_factor'] = float(df_final.loc[index, 'seasonal'])
         value_list.append(value_entry)
 
     json_dict = {}
@@ -237,9 +233,9 @@ def get_forecasts():
     value_list = []
     for index, row in df_final.iterrows():
         value_entry = {}
-        value_entry['trip_year'] = int(df_final.loc[index, 'Trip Year'])
-        value_entry['trip_month'] = int(df_final.loc[index, 'Trip Month'])
-        value_entry['trips'] = int(df_final.loc[index, 'Trips'])
+        value_entry['trip_year'] = int(df_final.loc[index, 'trip_year'])
+        value_entry['trip_month'] = int(df_final.loc[index, 'trip_month'])
+        value_entry['trips'] = int(df_final.loc[index, 'trips'])
         value_list.append(value_entry)
 
     json_dict = {}
