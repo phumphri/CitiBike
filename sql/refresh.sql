86886012270a412bf44652d19750b71381f50567
pvhumphr <pvhumphr@usc.edu>
2018-07-24 10:28:52 -0700

    Post build of materialized views.

---
+++ sql/refresh.sql
@@ -0,0 +1,9 @@
+REFRESH MATERIALIZED VIEW citibike.age_minutes_by_year_month_day 	WITH DATA;
+REFRESH MATERIALIZED VIEW citibike.gender_trips_by_year_month_day	WITH DATA;
+REFRESH MATERIALIZED VIEW citibike.start_stations_with_duplicates	WITH DATA;
+REFRESH MATERIALIZED VIEW citibike.stop_stations_with_duplicates 	WITH DATA;
+REFRESH MATERIALIZED VIEW citibike.trips_by_year_minutes_gender_age WITH DATA;
+REFRESH MATERIALIZED VIEW citibike.trips_by_year_month 				WITH DATA;
+REFRESH MATERIALIZED VIEW citibike.trips_by_year_month_day 			WITH DATA;
+REFRESH MATERIALIZED VIEW citibike.trips_by_year_season_hour 		WITH DATA;
+REFRESH MATERIALIZED VIEW citibike.user_type_by_year_month_day 		WITH DATA;
